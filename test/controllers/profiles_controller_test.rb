c088d8459e34153db33f24d690acde051eaa2d3b
Jorge Coutinho <jorgecsn@id.uff.br>
2025-01-12 16:07:38 -0300

    remove rspec

--- test/controllers/profiles_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ProfilesControllerTest < ActionDispatch::IntegrationTest
-  test "should get profile" do
-    get profiles_profile_url
-    assert_response :success
-  end
-end
